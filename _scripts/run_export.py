3462e75c6282224a71321405b3a5814691dd63c9
codaley <coreyelijah@gmail.com>
2024-12-15 13:20:28 -0800

    Update run_export.py
    Signed-off-by: codaley <coreyelijah@gmail.com>

--- _scripts/run_export.py
+++ _scripts/run_export.py
@@ -6,15 +6,17 @@ def main():
     parser.add_argument("--weights", type=str, required=True, help="Path to the trained weights (e.g., runs/train/exp/weights/best.pt)")
     parser.add_argument("--device", type=str, default="cuda:0", help="Device for export (default: cuda:0)")
     parser.add_argument("--include", type=str, default="tflite", help="Format to export (default: tflite)")
+    parser.add_argument("--img", type=int, default=320, help="Export image size (default: 320)")
 
     args = parser.parse_args()
 
     # Construct the YOLOv5 export command
     command = [
-        "python", "yolov5/export.py",
+        "python", "export.py",
         "--weights", args.weights,
         "--device", args.device,
-        "--include", args.include
+        "--include", args.include,
+        "--img", str(args.img)
     ]
 
     # Execute the command
